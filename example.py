8f59a5c67216a2215957e6850759030a7828bb7d
Joachim Brod <joachim.m.brod@gmail.com>
2017-10-21 15:32:08 +0200

    Added examples for new dim.7 operators in example.py

--- example.py
+++ example.py
@@ -105,5 +105,43 @@ print('Match from five-flavor to four-flavor theory at scale 10 GeV:\n')
 print(wc5f.match(mu=10))
 print('\n')
 
+print('\n')
+print('-----------------------------------------')
+print('\n')
+
+
+
+
+#---------------------------------------------#
+# Examples with new dimension-seven operators #
+#---------------------------------------------#
+
+
+# Low-energy coefficients for dimension-seven operators with derivatives
+
+dict7 = {'C715u' : 1./scale**3, 'C715d' : 1./scale**3, 'C715s' : 1./scale**3, 'C716u' : 1./scale**3, 'C716d' : 1./scale**3, 'C716s' : 1./scale**3, }
+wc_7 = ddm.WC_5f(dict7, DM_type="M")
+
+
+print('Low-energy coefficients from dimension-seven operators, Majorana DM:\n')
+print(wc_7.cNR(100, 50e-3))
+print('\n')
+
+
+
+# Rayleigh operators currently only get a matrix element from QED mixing into scalar-current operators:
+
+dict_rayleigh = {'C711' : 1./scale**3, 'C712' : 1./scale**3, 'C713' : 1./scale**3, 'C714' : 1./scale**3}
+wc_rayleigh = ddm.WC_5f(dict_rayleigh, DM_type="D")
+
+
+print('Low-energy coefficients from Rayleigh operators, from QED mixing:\n')
+print(wc_rayleigh.cNR(100, 50e-3, RGE=True))
+print('\n')
+print('If you switch of QED, the low-energy coefficients are zero:\n')
+print(wc_rayleigh.cNR(100, 50e-3, RGE=False))
+print('\n')
+
+
 sys.exit()
 
